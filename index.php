f138aac7f87d619a5e05d52656bf6814155f921a
qianjinding <qianjinding.git@gmail.com>
2013-06-17 11:44:39 +0400

    first blood?

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+    
+</head>
+<body>
+    Hello World!
+</body>
+</html>
